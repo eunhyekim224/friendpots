b234d040d2730822a173faa54e0332ff87cfb486
Eunhye Kim <kimeunhye224@gmail.com>
2021-06-23 20:44:46 +0100

    add one basic method

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,25 @@
-import http from "http";
+import http, { IncomingMessage, ServerResponse } from "http";
+import { URL } from "url";
 
 const hostname = "127.0.0.1";
 const port = 8000;
 
-// functio
+function handler(req: IncomingMessage, res: ServerResponse) {
+  console.log(req.url);
+  const parsedUrl = new URL(req.url as string, `http://${hostname}:${port}`);
 
-const server = http.createServer((req, res) => {
-    res.end()
-});
+  if (parsedUrl.pathname === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World");
+  } else if (parsedUrl.pathname === "/friend") {
+    // write to file
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("No such path exists");
+  }
+}
+
+const server = http.createServer(handler);
 
 server.listen(port, hostname, () => {
   console.log(`Server is running on http://${hostname}:${port}`);
