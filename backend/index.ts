26122bc7b5cb61efd9c4ef80bf9a089d5998b55c
Eunhye Kim <kimeunhye224@gmail.com>
2021-06-29 19:42:53 +0100

    WIP

--- backend/index.ts
+++ backend/index.ts
@@ -1,39 +1,67 @@
 import http, { IncomingMessage, ServerResponse } from "http";
 import { URL } from "url";
 import fs from "fs";
+import { uuid } from "uuidv4";
 
 const hostname = "127.0.0.1";
 const port = 8000;
 
 type Friend = {
-    id?: string,
-    name: string
-}
+  id?: string;
+  name: string;
+};
 
-type GetCallback = {
-    <T>(req: IncomingMessage, reqUrl: URL): T
-}
+type GetCallback<T> = {
+  (req: IncomingMessage, reqUrl: URL): T;
+};
 
-function createNewFriend(friendData: Friend) {
+type PostCallback<T> = {
+  (postedData: T): T;
+};
+
+function createNewFriend(friendData: Friend): Friend {
+  const newFriend = {
+    id: uuid(),
+    name: friendData.name,
+  } as Friend;
   // store friend to a file
-  fs.readFile(
-    "store/friends.json",
-    "utf8",
-    function readFileCallback(err, data) {
-      if (err) {
-        console.log(err);
-      } else {
-        const allFriends = JSON.parse(data);
-        allFriends.push(friendData);
-        const allFriendsJson = JSON.stringify(allFriends);
-        fs.writeFileSync("store/friends.json", allFriendsJson);
-      }
+  fs.readFile("store/friends.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const allFriends = JSON.parse(data);
+      allFriends.push(friendData);
+      const allFriendsJson = JSON.stringify(allFriends);
+      fs.writeFileSync("store/friends.json", allFriendsJson);
     }
-  );
+  });
+  return {} as Friend;
 }
 
-function getHandler<T>(req: IncomingMessage, res: ServerResponse, reqUrl: URL, callback: GetCallback) {
-  const getResponse = callback<T>(req, reqUrl);
+function postHandler<T>(
+  req: IncomingMessage,
+  res: ServerResponse,
+  callback: PostCallback<T>
+) {
+  req.setEncoding("utf8");
+  let data = "";
+  req.on("data", (chunk: string) => {
+    data += chunk;
+  });
+  req.on("end", () => {
+    const postedData = JSON.parse(data) as T;
+    const postResponse = callback(postedData);
+    res.end(JSON.stringify(postResponse));
+  });
+}
+
+function getHandler<T>(
+  req: IncomingMessage,
+  res: ServerResponse,
+  reqUrl: URL,
+  callback: GetCallback<T>
+) {
+  const getResponse = callback(req, reqUrl);
   if (getResponse) {
     res.writeHead(200);
     res.end(JSON.stringify(getResponse));
@@ -42,19 +70,6 @@ function getHandler<T>(req: IncomingMessage, res: ServerResponse, reqUrl: URL, c
     res.end();
   }
   res.end();
-}
-
-function postHandler(req: IncomingMessage, res: ServerResponse, callback: any) {
-  req.setEncoding("utf8");
-  let data = "";
-  req.on("data", (chunk: string) => {
-    data += chunk;
-  });
-  req.on("end", () => {
-    const postedData = JSON.parse(data);
-    callback(postedData);
-    res.end();
-  });
 }
 
 function noResponse(req: IncomingMessage, res: ServerResponse) {
@@ -72,9 +87,10 @@ function handler(req: IncomingMessage, res: ServerResponse) {
       res.end("Hello World");
       break;
     case "/friend":
-      postHandler(req, res, createNewFriend);
+      postHandler<Friend>(req, res, (f) => createNewFriend(f));
       break;
-    default: noResponse(req, res);
+    default:
+      noResponse(req, res);
   }
 }
 
