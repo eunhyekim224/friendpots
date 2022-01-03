6e8b6960683b3c96389537aeff2556cc77200f8c
Eunhye Kim <kimeunhye224@gmail.com>
2022-01-03 12:06:50 +0000

    create a Store object

--- backend/src/api/components/friend/controller.ts
+++ backend/src/api/components/friend/controller.ts
@@ -1,47 +1,23 @@
-import { IncomingMessage } from 'http';
-import { Friend } from './model/Friend';
-import { Friends } from './model/Friends';
-import fs from "fs";
+import { IncomingMessage } from "http";
+import { Friend } from "./model/Friend";
+import { Friends } from "./model/Friends";
+import { Store } from "../../../../store/Store";
 
 export class FriendController {
+    friendStorePath = "./backend/store/friends.json";
 
     createNewFriend(friendData: FriendDTO): Promise<Friend> {
         const newFriend = new Friends().create(friendData.name);
 
-        // store friend to a file
-        return new Promise((resolve, reject) => {
-            fs.readFile("./backend/store/friends.json", "utf8", (err, data) => {
-                if (err) {
-                    throw err;
-                } else {
-                    const allFriends = JSON.parse(data);
-                    allFriends.push(newFriend);
-
-                    const allFriendsJson = JSON.stringify(allFriends);
-                    fs.writeFileSync("./backend/store/friends.json", allFriendsJson);
-                }
-            });
-            resolve(newFriend);
-        });
+        const store = new Store();
+        return store.write<Friend>(this.friendStorePath, newFriend);
     }
 
     getFriend(req: IncomingMessage, reqUrl: URL): Promise<Friend> {
         const friendId = reqUrl.pathname.split("/")[2];
-        
-        return new Promise((resolve, reject) => {
-            fs.readFile("./backend/store/friends.json", "utf8", (err, data) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    const allFriends = JSON.parse(data);
-                    const friendToRetrieve = allFriends.find(
-                        (friend: Friend) => friend.id === friendId
-                    );
-                    console.log("FRIEND", friendToRetrieve);
-                    resolve(friendToRetrieve);
-                }
-            });
-        });
+
+        const store = new Store();
+        return store.read<Friend>(this.friendStorePath, friendId);
     }
 }
 
