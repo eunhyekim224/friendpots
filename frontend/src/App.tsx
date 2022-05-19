e5895b1dc00e1e4b902ef5bde5eefabd9b2b0feb
Eunhye Kim <kimeunhye224@gmail.com>
2022-05-19 19:18:39 +0100

    update homescreen ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,13 +9,10 @@ function App() {
         event.preventDefault();
 
         try {
-            const { data: addedFriend } = await axios.post(
-                "/friends",
-                {
-                    name,
-                }
-            );
-            alert(`Your friend, ${name}, has just been planted!`)
+            const { data: addedFriend } = await axios.post("/friends", {
+                name,
+            });
+            alert(`Your friend, ${name}, has just been planted!`);
         } catch (err) {
             console.log(err);
         }
@@ -23,23 +20,23 @@ function App() {
 
     return (
         <div className="App">
-            <header className="App-header">
-                <p>Welcome to FriendPots!</p>
-                <div>
-                    <form onSubmit={addFriend}>
-                        <label>
-                            Enter your friend's name:
-                            <input
-                                type="text"
-                                name="name"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                            />
-                        </label>
-                        <input type="submit" value="Add" />
-                    </form>
-                </div>
-            </header>
+            <div className="App-header">
+                <h1 id="header-text">FriendPots</h1>
+            </div>
+            <div>
+                <form onSubmit={addFriend}>
+                    <label>
+                        Enter your friend's name:
+                        <input
+                            type="text"
+                            name="name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </label>
+                    <input type="submit" value="Add" />
+                </form>
+            </div>
         </div>
     );
 }
