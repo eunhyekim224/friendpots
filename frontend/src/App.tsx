06f853b435a554ecd11961bf172e59aea517373b
Eunhye Kim <kimeunhye224@gmail.com>
2022-05-27 20:01:32 +0100

    retrieve friend and show

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,62 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import { NewFriendForm } from "./components/newFriendForm";
 import Modal from "react-modal";
 
+Modal.setAppElement("#root");
+
+type Friend = {
+    id?: string;
+    name: string;
+};
+
 function App() {
-    const [modalIsOpen, setIsOpen] = React.useState(false);
+    const [modalIsOpen, setIsOpen] = useState(false);
+    const [newFriendId, setNewFriendId] = useState();
+    const [newFriend, setNewFriend] = useState<null | Friend>();
+
+    console.log(" state newFriendId", newFriendId);
 
     const addFriend = async (event: any) => {
         event.preventDefault();
 
-        const name = event.target.name.value
-
-        const newFriend = {
-            name
-        }
+        const name = event.target.name.value;
+        const newFriend: Friend = {
+            name,
+        };
 
         try {
-            const { data: addedFriend } = await axios.post("/friends", newFriend);
-            alert(`Your friend, ${addedFriend.name}, has just been planted!`);
+            const { data: addedFriend } = await axios.post(
+                "/friends",
+                newFriend
+            );
+            setNewFriendId(addedFriend.id);
+            console.log("newFriendId", newFriendId);
         } catch (err) {
-            console.error(err);
+            console.error("Failed to create new friend", err);
         }
     };
+
+    const getNewFriend = async () => {
+        console.log("getNewFriend start");
+        // try {
+            const { data: friend } = await axios.get(`friends/${newFriendId}`);
+                    console.log("getNewFriend end");
+
+            console.log("friend retrieved", friend);
+
+            setNewFriend(friend);
+        // } catch (err) {
+        //     console.error("Failed to fetch new friend", err);
+        // }
+    };
+
+    useEffect(() => {
+        if (!newFriend && newFriendId) {
+            getNewFriend();
+        }
+    }, [getNewFriend, newFriend]);
 
     function openModal() {
         setIsOpen(true);
@@ -57,11 +91,19 @@ function App() {
                     style={customStyles}
                 >
                     <div>
-                        <NewFriendForm onSubmit={addFriend}></NewFriendForm>
+                        <NewFriendForm
+                            onSubmit={async (e) => await addFriend(e)}
+                        ></NewFriendForm>
                     </div>
                     <button onClick={closeModal}>close</button>
                 </Modal>
             </div>
+            {newFriend && (
+                <div>
+                    <h2>Your New FriendPot!</h2>
+                    <p>{newFriend.name}</p>
+                </div>
+            )}
         </div>
     );
 }
