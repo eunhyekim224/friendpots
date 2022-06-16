b8031c08d7d7ab656447206cb9bd6e0324f90b17
Eunhye Kim <kimeunhye224@gmail.com>
2022-06-16 20:15:37 +0100

    add unit test WIP

--- web/src/screens/Home/Home.test.tsx
+++ web/src/screens/Home/Home.test.tsx
@@ -1,9 +1,27 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Home } from "./Home";
+import axios from "axios";
+import userEvent from '@testing-library/user-event';
+
 
 describe("Home", () => {
-    it("should render correctly", () => {
-        const tree = render(<Home />);
-        expect(tree).toMatchSnapshot();
+    it("allows user to add a new friendpot successfully", () => {
+        const newFriendName = 'friend name'
+        const fakeAddFriendResponse = { id: 'addedFriendId', name: newFriendName }
+        jest.spyOn(axios, 'post').mockImplementation(() => {
+            return Promise.resolve({
+                json: () => Promise.resolve(fakeAddFriendResponse)
+            })
+        })
+
+        render(<Home />);
+
+        userEvent.click(screen.getByText(/new/i));
+
+        userEvent.type(screen.getByLabelText(/name/i), newFriendName);
+
+        userEvent.click(screen.getByText(/add/i));
+
+        
     });
 });
