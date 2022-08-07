3db0d94a9f34aa6d91c53380235df65905970452
Eunhye Kim <kimeunhye224@gmail.com>
2022-08-07 17:15:39 +0100

    fix path

--- api/src/friend/Friends.ts
+++ api/src/friend/Friends.ts
@@ -1,9 +1,10 @@
+import path, { dirname } from 'path';
 import { Store } from "../Store";
 import { Friend } from "./Friend";
 
 export class Friends {
+    friendStorePath = __dirname + `/../../store/friends.json`; // path resolve 
 
-    friendStorePath = "./api/store/friends.json";
     store = new Store<Friend>(this.friendStorePath);
 
     save(friend: Friend): Promise<Friend> {
