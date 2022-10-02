68044bf5b77e1ad8cb9bc1a87f441424be41fde1
Eunhye Kim <kimeunhye224@gmail.com>
2022-10-02 12:52:17 +0100

    add a dialog for login

--- web/src/screens/Home/Home.tsx
+++ web/src/screens/Home/Home.tsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { ReactElement, useCallback, useEffect, useState } from "react";
 import "./Home.styled";
-import { Box, Typography } from "@mui/material";
+import { Box, Modal, Typography } from "@mui/material";
 import { AddFriendButton } from "./Home.styled";
 import { FriendPot } from "./components/FriendPot";
 import { AddFriendFormDialog } from "./components/AddFriendFormDialog";
 import { StatusSnackbar } from "../../molecules/StatusSnackbar/StatusSnackbar";
+import { LoginDialog } from "./components/LoginDialog";
 
 type Friend = {
     id?: string;
@@ -13,6 +14,8 @@ type Friend = {
 };
 
 export const Home = (): ReactElement => {
+    const [userId, setUserId] = useState("");
+    const [isLoginModalOpen, setLoginModalOpen] = useState(false);
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const [newFriend, setNewFriend] = useState<Friend>();
     const [snackbarStatus, setSnackbarStatus] = useState<string>();
@@ -59,7 +62,15 @@ export const Home = (): ReactElement => {
         [newFriend?.id]
     );
 
+    const showLoginModal = () => {
+        setLoginModalOpen(true);
+    };
+
     useEffect(() => {
+        if (!userId) {
+            showLoginModal();
+        }
+
         if (newFriend?.id) {
             getFriend(newFriend.id);
         }
@@ -73,9 +84,50 @@ export const Home = (): ReactElement => {
         setModalIsOpen(false);
     };
 
+    const closeLoginModal = () => {
+        setLoginModalOpen(false);
+    };
+
     const handleSnackbarClose = () => {
         setSnackbarIsOpen(false);
     };
+
+    const loginModalStyle = {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: 400,
+        bgcolor: "background.paper",
+        border: "2px solid #000",
+        boxShadow: 24,
+        p: 4,
+    };
+
+    // const LoginModal = (): ReactElement => {
+    //     return (
+    //         <Modal
+    //             open={isLoginModalOpen}
+    //             onClose={closeLoginModal}
+    //             aria-labelledby="modal-modal-title"
+    //             aria-describedby="modal-modal-description"
+    //         >
+    //             <Box sx={loginModalStyle}>
+    //                 <Typography
+    //                     id="modal-modal-title"
+    //                     variant="h6"
+    //                     component="h2"
+    //                 >
+    //                     Enter your unique email:
+    //                 </Typography>
+    //                 <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+    //                     Duis mollis, est non commodo luctus, nisi erat porttitor
+    //                     ligula.
+    //                 </Typography>
+    //             </Box>
+    //         </Modal>
+    //     );
+    // };
 
     return (
         <Box
@@ -83,6 +135,13 @@ export const Home = (): ReactElement => {
                 backgroundColor: "#7cb342",
             }}
         >
+            {isLoginModalOpen && (
+                <LoginDialog
+                    isOpen={isLoginModalOpen}
+                    close={closeLoginModal}
+                    // login={(userId) => {}}
+                />
+            )}
             <Typography
                 variant="h1"
                 component="div"
---
+++ web/src/screens/Home/components/LoginDialog.tsx
@@ -0,0 +1,68 @@
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    TextField,
+} from "@mui/material";
+import { useState } from "react";
+
+type LoginDialogProps = {
+    isOpen: boolean;
+    close: () => void;
+    // login: (userId: string) => Promise<void>;
+};
+
+export const LoginDialog = (props: LoginDialogProps): JSX.Element => {
+    const [userId, setUserId] = useState<string>("");
+
+    const handleUserIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUserId(event.target.value);
+    };
+
+    // const handleKeyPress = async (event: React.KeyboardEvent) => {
+    //     if (event.key === "Enter") {
+    //         await props.login(userId);
+    //     }
+    // };
+
+    return (
+        <Dialog
+            open={props.isOpen}
+            onClose={props.close}
+            maxWidth="sm"
+            fullWidth={true}
+        >
+            <DialogTitle sx={{ color: "green" }}>Welcome to Friendpots!</DialogTitle>
+
+            <DialogContent>
+                <DialogContentText>
+                    To add and view your friendpots, please enter your email address here. 
+                </DialogContentText>
+                <TextField
+                    autoFocus={true}
+                    margin="dense"
+                    id="name"
+                    label="e.g. friendpots@gmail.com"
+                    type="text"
+                    variant="standard"
+                    fullWidth
+                    color="success"
+                    onChange={handleUserIdChange}
+                    // onKeyDown={handleKeyPress}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button
+                    id="add-button"
+                    sx={{ color: "green" }}
+                    // onClick={async () => await props.login(name)}
+                >
+                    Enter
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
