a80223889b9bfecbf7215dbc21fea0da7cbe9edc
Eunhye Kim <kimeunhye224@gmail.com>
2022-11-04 18:59:09 +0000

    update api test

--- api/src/tests/friends.spec.ts
+++ api/src/tests/friends.spec.ts
@@ -1,6 +1,7 @@
 import fetch from "node-fetch";
 import { describe, it } from "mocha";
 import assert from "assert";
+import { v4 as uuidv4 } from "uuid";
 
 type FriendDTO = {
     userId: string;
@@ -12,7 +13,7 @@ describe("Friends", () => {
         it("should create a new friend", async () => {
             // Arrange
             const testFriendDto: FriendDTO = {
-                userId: "test@gmail.com",
+                userId: `${uuidv4()}@gmail.com}`,
                 name: "New Test Friend",
             };
 
@@ -29,30 +30,22 @@ describe("Friends", () => {
             );
             const testFriend = await createNewFriendResponse.json();
 
-            const friendLocation =
-                createNewFriendResponse.headers.get("Location");
-
-            const getFriendResponse = await fetch(
-                `http://localhost:8000/${friendLocation}`
+            const getFriendsResponse = await fetch(
+                `http://localhost:8000/friends?userId=${testFriendDto.userId}`
             );
 
-            console.log(
-                "location",
-                createNewFriendResponse.headers.get("Location")
-            );
-
-            const createdFriend = await getFriendResponse.json();
+            const friends = await getFriendsResponse.json();
 
             // Assert
             const postFriendIsSuccessful =
                 createNewFriendResponse.status === 201;
-            const getFriendIsSuccessful = getFriendResponse.status === 200;
+            const getFriendIsSuccessful = getFriendsResponse.status === 200;
 
             assert.strictEqual(postFriendIsSuccessful, true);
 
             assert.strictEqual(getFriendIsSuccessful, true);
 
-            assert.deepStrictEqual(testFriend, createdFriend);
+            assert.deepStrictEqual(testFriend, friends[0]);
         });
     });
 });
