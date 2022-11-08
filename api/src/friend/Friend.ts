3b0a3ca60b0d90c3662cb7acb7273721296262db
Eunhye Kim <kimeunhye224@gmail.com>
2022-11-08 21:30:02 +0000

    add props to friend object

--- api/src/friend/Friend.ts
+++ api/src/friend/Friend.ts
@@ -2,10 +2,14 @@ export class Friend {
     id;
     userId;
     name;
+    hardiness;
+    state;
 
-    constructor(id: string, userId: string, name?: string) {
+    constructor(id: string, userId: string, name?: string, hardiness?: string, state?: string) {
         this.id = id;
         this.userId = userId;
         this.name = name;
+        this.hardiness = hardiness;
+        this.state = state;
     }
 }
